552a2890b3022916b3324c8c19fd0102e06d78cd
notpunchnox <punchnoxpaypal@gmail.com>
2024-07-20 23:34:30 +0200

    Add tags

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const prompt = "C'est quoi les robots hexapodes ?";
 console.log('\n\x1b[1mQuestion:\x1b[0m\x1b[36m', prompt, '\x1b[0m\n');
 
 /* Fonction Question */
-const result = await Question(prompt, 6)
+const result = await Question(prompt, 2)
 
 /* Afficher la réponse */
 console.log('\n\x1b[1mRéponse:\x1b[0m\x1b[36m', result, '\x1b[0m');
